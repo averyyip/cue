4c0c611b63be05e583879aee187f36d5671f2111
Annie Zheng <anzheng@berkeley.edu>
2020-04-24 23:04:07 -0700

    basic fonts

---
+++ frontend/components/BaseComponents.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+import * as Fonts from '@assets/fonts/Fonts';
+
+// store names, bold informational text
+export const boldHeader = styled.Text`
+  font-size: 20px;
+  font-family: sf-pro-display-bold;
+`;
+
+// carousel headers
+export const smallHeader = styled.Text`
+  font-size: 16px;
+  font-family: sf-pro-display-bold;
+`;
+
+// green store name
+export const tinyHeader = styled.Text`
+  font-size: 12px;
+  font-family: sf-pro-display-bold;
+`;
+
+// button text
+export const buttonText = styled.Text`
+  font-size: 20px;
+  font-family: sf-pro-display-bold;
+`;
+
+// card descriptions
+export const bodyText = styled.Text`
+  font-size: 20px;
+  font-family: sf-pro-display-regular;
+`;
+
+// store address
+export const addressText = styled.Text`
+  font-size: 14px;
+  font-family: sf-pro-display-regular;
+`;
--- frontend/components/CarouselCard.jsx
+++ frontend/components/CarouselCard.jsx
@@ -2,3 +2,22 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { Colors } from '@assets/Colors';
 import * as Fonts from '@assets/fonts/Fonts';
+
+export default class CarouselCard extends React.Component<CardProps> {
+  render() {
+    return (
+      <YellowCard />
+    );
+  }
+}
+
+export const YellowCard = styled.View`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: ${Colors.Yellow};
+  justify-content: flex-start;
+  border-radius: 5px;
+  padding: 5%;
+  height: 190px;
+`;
