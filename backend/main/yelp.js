7d025a5746f2521afb919485d45ae967d563dad3
Avery <averyyip@berkeley.edu>
2020-04-25 10:42:21 -0700

    Working yelp

--- backend/main/yelp.js
+++ backend/main/yelp.js
@@ -1,1 +1,20 @@
-const axios = require('axios').default;
+const yelp = require('yelp-fusion');
+const env = require('../env');
+
+const client = yelp.client(env.yelp_api_key);
+
+async function getClosestStore(lon, lat) {
+  const params = {
+    term: 'grocery stores',
+    longitude: lon,
+    latitude: lat,
+  };
+
+  try {
+    const res = await client.search(params);
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
+}
+getClosestStore(-122.2540443, 37.870456);
