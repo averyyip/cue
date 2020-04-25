c03c6bcde859ed698f23ec54d549c24042e53c24
Avery <averyyip@berkeley.edu>
2020-04-25 08:41:02 -0700

    BE(backend) - completed test endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const controllers = require('./controllers/index');
 
-const router = express.Router();
+const app = express();
 
 // Home page route.
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
   const result = controllers.testController.getTest(req, res);
   res.send(result);
 });
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
