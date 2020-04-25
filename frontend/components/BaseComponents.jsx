f12c632c513cd54ab8014ed9c24902c4638786bc
Annie Zheng <anzheng@berkeley.edu>
2020-04-25 00:07:40 -0700

    carousel cards

--- frontend/components/BaseComponents.jsx
+++ frontend/components/BaseComponents.jsx
@@ -2,37 +2,37 @@ import styled from 'styled-components/native';
 import * as Fonts from '@assets/fonts/Fonts';
 
 // store names, bold informational text
-export const boldHeader = styled.Text`
+export const BoldHeader = styled.Text`
   font-size: 20px;
   font-family: sf-pro-display-bold;
 `;
 
 // carousel headers
-export const smallHeader = styled.Text`
+export const SmallHeader = styled.Text`
   font-size: 16px;
   font-family: sf-pro-display-bold;
 `;
 
 // green store name
-export const tinyHeader = styled.Text`
+export const TinyHeader = styled.Text`
   font-size: 12px;
   font-family: sf-pro-display-bold;
 `;
 
 // button text
-export const buttonText = styled.Text`
+export const ButtonText = styled.Text`
   font-size: 20px;
   font-family: sf-pro-display-bold;
 `;
 
 // card descriptions
-export const bodyText = styled.Text`
+export const BodyText = styled.Text`
   font-size: 20px;
   font-family: sf-pro-display-regular;
 `;
 
 // store address
-export const addressText = styled.Text`
+export const AddressText = styled.Text`
   font-size: 14px;
   font-family: sf-pro-display-regular;
 `;
--- frontend/components/CarouselCard.jsx
+++ frontend/components/CarouselCard.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { Colors } from '@assets/Colors';
 import * as Fonts from '@assets/fonts/Fonts';
+import * as BaseComponents from '@components/BaseComponents';
+import { Image } from 'react-native';
 
-export default class CarouselCard extends React.Component<CardProps> {
+function getCardType(name: string) {
+  ['smallbusiness']: {
+    title: 'Support local businesses!',
+    imageUri: '@assets/images/small-business.png',
+    body: 'Shop at smaller grocery stores to support local businesses. Often, those stores have shorter wait times and plentiful inventory!'
+  }
+}
+
+export default class CarouselCard(cardType) extends React.Component<CardProps> {
+  cardDetails = getCardType(cardType);
+
   render() {
     return (
-      <YellowCard />
+      <YellowCard>
+        <BaseComponents.SmallHeader>{cardDetails.title}</BaseComponents.SmallHeader>
+        <Image
+          style={width: 50,height: 50,}
+          source={require({cardDetails.imageUri})}
+        />
+        <BaseComponents.BodyText>{cardDetails.body}</BaseComponents.BodyText>
+      </YellowCard>
     );
   }
 }
