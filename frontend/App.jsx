63c29d8083ac3d695e72e192fcdd766b5877c2c4
Avery Yip <averyyip@berkeley.edu>
2020-04-25 21:59:55 -0700

    Annie/navigation (#10)
    * stack navigator wip
    
    * blank screens
    
    * stack
    
    * not displaying
    
    * Removed view from animation
    
    Co-authored-by: Annie Zheng <anzheng@berkeley.edu>

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import * as Font from 'expo-font';
 import { setCustomText } from 'react-native-global-props';
-import { StyleSheet, View } from 'react-native';
 import { Fonts } from './assets/fonts/Fonts';
-import * as BottomButton from './components/BottomButton';
-import { LeaveWaitlistCard } from './components/LeaveWaitlistCard';
-
+import { AppContainer } from './components/Navigation';
 
 export default class App extends React.Component {
   constructor() {
@@ -37,16 +34,7 @@ export default class App extends React.Component {
       return null;
     }
     return (
-      <View />
+      <AppContainer />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
